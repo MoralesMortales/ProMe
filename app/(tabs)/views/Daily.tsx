18c49a06aa05a5a33d547f4c22ce468c9d18fd53
MoralesMortales <carlos52.7624@gmail.com>
2025-07-08 09:26:25 -0500

    commit base

--- app/(tabs)/views/Daily.tsx
+++ app/(tabs)/views/Daily.tsx
@@ -1,10 +1,30 @@
-import { StatusBar, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function Daily() {
   return (
     <View>
-      <StatusBar />
-      <Text>Holua Dayli</Text>
+      <StatusBar/>
+      <View style={styles.baseHeader}>
+              <Text>Holua Dayli</Text>
+      </View>
+      <View>
+              <Text>Holua Dayli</Text>
+
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  baseHeader: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    height: 24,
+    backgroundColor: '#e3e',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
