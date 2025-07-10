034168959e014a685299cc17b407bccf8b417b98
MoralesMortales <carlos52.7624@gmail.com>
2025-07-10 18:50:59 -0500

    cssing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,54 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { icons } from "@/constants/icons";
+import { Image, StyleSheet } from "react-native";
+
+const themeColor = "#000";
 
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "home", headerShown: false }}
+        options={{
+          title: "home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Image style={styles.tab} source={icons.Sun} tintColor={themeColor} />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Calendar"
+        options={{ title: "Calendar", headerShown: false }}
+      />
+
+      <Tabs.Screen
+        name="Daily"
+        options={{ title: "Daily", headerShown: false }}
+      />
+
+      <Tabs.Screen name="Add" options={{ title: "Add", headerShown: false }} />
+
+      <Tabs.Screen
+        name="Diary"
+        options={{ title: "Diary", headerShown: false }}
+      />
+
+      <Tabs.Screen
+        name="Settings"
+        options={{ title: "Settings", headerShown: false }}
       />
     </Tabs>
   );
 };
 
 export default _layout;
+
+const styles = StyleSheet.create({
+  tab: {
+    backgroundColor: "#000",
+  },
+});
---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+import Book from "@/assets/icons/Book.png";
+import Calendar from "@/assets/icons/Calendar.png";
+import Plus from "@/assets/icons/Plus.png";
+import Settings from "@/assets/icons/Settings.png";
+import Sun from "@/assets/icons/Sun.png";
+
+export const icons = {
+  Book,
+  Calendar,
+  Plus,
+  Sun,
+  Settings,
+};
