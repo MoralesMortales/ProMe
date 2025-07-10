c674821eab02799e4003fb9e57daf81dcf3327aa
MoralesMortales <carlos52.7624@gmail.com>
2025-07-10 18:02:42 -0500

    changingParameter

---
+++ app/(tabs)/Add.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+export default function Add() {
+  return (
+    <View>
+      <Text>Add</Text>
+      <Link href={"./index"}>a</Link>
+    </View>
+  )
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,17 @@
-import { Redirect } from 'expo-router';
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  return <Redirect href="/(tabs)/views/Daily" />; 
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Go to daily</Text>
+      <Link href={"./"}>About</Link>
+    </View>
+  );
 }
