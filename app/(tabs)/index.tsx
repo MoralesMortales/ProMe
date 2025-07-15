8af5c44e0017c725370e15ca3460bd7b41a4a773
MoralesMortales <carlos52.7624@gmail.com>
2025-07-14 20:06:14 -0500

    mainTabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,65 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import WeekCarrousel from "../../components/WeekCarrousel"
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import WeekCarrousel from "../../components/WeekCarrousel";
+import { useState } from "react";
 
 export default function Index() {
+  const [selectedMainTab, setMainTab] = useState(null);
+
   return (
     <View>
+      <StatusBar />
       <Text style={styles.title}>My Tasks</Text>
       <View>
-        <WeekCarrousel/>
+        <WeekCarrousel />
+      </View>
+      <View style={styles.mainTabsContainer}>
+        <TouchableOpacity
+          style={styles.mainTabs}
+          onPress={() => setMainTab("habits")}
+        >
+          <Text>Habits</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.mainTabs}
+          onPress={() => setMainTab("tasks")}
+        >
+          <Text>Task</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-    title: {
-        marginTop:30,
-    marginLeft:7,
-    fontSize:23,
-    },
-});
+const themeColor = "#555";
+const activeThemeColor = "#888";
+const passiveThemeColor = "#aaa";
+const dark = false;
 
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 20,
+    marginLeft: 7,
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  mainTabsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+  },
+  mainTabs: {
+    backgroundColor: activeThemeColor,
+    paddingVertical: 2,
+    width: "40%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius:90
+  },
+});
